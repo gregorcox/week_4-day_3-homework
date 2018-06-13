499ee93fa63201555dbfa944886d22956c53e73e
Gregor Cox <gregorcox@gmail.com>
2018-06-13 14:34:42 +0100

    initial commit

---
+++ pizza_controller.rb
@@ -0,0 +1,29 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry' )
+require_relative( './models/pizza_order.rb' )
+also_reload( './models/*' )
+
+#INDEX
+get '/pizza-orders' do
+  @orders = PizzaOrder.all()
+  erb( :index )
+end
+
+#NEW
+get '/pizza-orders/new' do
+  erb( :new )
+end
+
+#SHOW
+get '/pizza-orders/:id' do
+  @order = PizzaOrder.find(params[:id].to_i)
+  erb ( :show )
+end
+
+#CREATE
+post '/pizza-orders' do
+  @order = PizzaOrder.new(params)
+  @order.save()
+  erb ( :create )
+end
